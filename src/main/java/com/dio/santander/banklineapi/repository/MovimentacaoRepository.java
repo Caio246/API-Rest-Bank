5068ea980443d3188a39715605b989f3cb377d1e
Caio Custódio Moreira <caiostun@gmail.com>
2022-05-06 02:15:33 -0300

    Enviando

---
+++ src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
@@ -0,0 +1,7 @@
+package com.dio.santander.banklineapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovimentacaoRepository extends JpaRepository<MovimentacaoRepository, Integer>{
+
+}
